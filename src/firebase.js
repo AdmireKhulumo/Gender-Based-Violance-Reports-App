8d567d03b856e6e2230e945176252bdb61595dd1
Admire Khulumo <admirekhulumo@gmail.com>
2020-10-29 00:41:36 +0200

    remove keys from firebase config file

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,14 @@
 import firebase from "firebase";
 
 const config = {
-    apiKey: "AIzaSyAeCuT71AuzFy9VZ3_on2aQxRAJxNc919E",
-    authDomain: "gbvbw-e1755.firebaseapp.com",
-    databaseURL: "https://gbvbw-e1755.firebaseio.com",
-    projectId: "gbvbw-e1755",
-    storageBucket: "gbvbw-e1755.appspot.com",
-    messagingSenderId: "165377936646",
-    appId: "1:165377936646:web:532a23a47000ebb734a287",
-    measurementId: "G-0C6LC96KE2"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
 };
 
 const firebaseApp = firebase.initializeApp(config);
